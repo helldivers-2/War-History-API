3d69c869e6b6c65226f1bd9b57ea13ef2cbd97fa
Kejax <kejax@kejax.net>
2024-03-26 20:03:19 +0100

    Deploy Automation

---
+++ .scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started!"
+
+# Enter maintenance mode
+(php artisan down) || true
+
+#
+git pull origin production
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreat cache
+php artisan optimize
+
+# Compile npm assets
+npm run build
+
+# Migrate the database
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
